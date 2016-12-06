f201e4d419e168e1dba838079e8a587687d08229
Paul Milne <hallhill@gmail.com>
2016-12-06 06:34:38 +0000

    added cat name and food list items and cat image

--- start_point/public/app.js
+++ start_point/public/app.js
@@ -1,3 +1,13 @@
 var catList = document.createElement('ul');
 catList.classList.add('cat');
 
+var catNameItem = document.createElement('li');
+catNameItem.innerText = "Minnie";
+
+var foodItem = document.createElement('li');
+foodItem.innerText = "Electrical cables";
+
+var imageSource = document.createElement('img');
+imageSource.src = "images/minnie.jpg";
+
+
